720f2000d76d4a6bdb0076c6e21b50c48b8a0f0a
benlaender <benito@bodtlander.com>
2024-05-24 13:55:20 +0200

    Extened Message 2.0

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('At least the docker deployment works. Azure does not like me, but Docker and Github like me ;D By Bodtlben');
+  res.send('At least the docker deployment works. Azure does not like me, but Docker and Github like me ;D By Bodtlben\n I did it!');
 });
 
 app.listen(PORT, HOST);
